db7ad83320099635d82c0e8e75606e2bdf3930fc
wangyuhao01@longfor.com <wangyuhao01@longfor.com>
2022-09-26 15:39:03 +0800

    新功能兼容历史版本IDEA

--- src/main/java/com/github/xiaolyuh/notify/NotifyUtil.java
+++ src/main/java/com/github/xiaolyuh/notify/NotifyUtil.java
@@ -1,9 +1,10 @@
 package com.github.xiaolyuh.notify;
 
+import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindowId;
 
 /**
  * 通知弹框
@@ -11,13 +12,22 @@ import com.intellij.openapi.project.Project;
  * @author yuhao.wang3
  */
 public class NotifyUtil {
-    private static final NotificationGroup TOOLWINDOW_NOTIFICATION = NotificationGroupManager.getInstance().getNotificationGroup("GitFlowPlusNotification");
-    private static final NotificationGroup BALLOON_NOTIFICATION = NotificationGroupManager.getInstance().getNotificationGroup("GitFlowPlusNotification");
-    private static final NotificationGroup NONE = NotificationGroupManager.getInstance().getNotificationGroup("GitFlowPlusNotification");
+    private static final NotificationGroup TOOLWINDOW_NOTIFICATION = NotificationGroup.toolWindowGroup(
+            "GitflowPlus Errors", ToolWindowId.VCS, true);
+    private static final NotificationGroup STICKY_NOTIFICATION = new NotificationGroup(
+            "GitflowPlus Errors", NotificationDisplayType.STICKY_BALLOON, true);
+    private static final NotificationGroup BALLOON_NOTIFICATION = new NotificationGroup(
+            "GitflowPlus Notifications", NotificationDisplayType.BALLOON, true);
 
+    private static final NotificationGroup NONE = new NotificationGroup(
+            "GitflowPlus Notifications", NotificationDisplayType.NONE, true);
 
     public static void notifySuccess(Project project, String title, String message) {
         notify(NotificationType.INFORMATION, BALLOON_NOTIFICATION, project, title, message);
+    }
+
+    public static void notifyInfo(Project project, String title, String message) {
+        notify(NotificationType.INFORMATION, TOOLWINDOW_NOTIFICATION, project, title, message);
     }
 
     public static void notifyError(Project project, String title, String message) {
@@ -25,7 +35,7 @@ public class NotifyUtil {
     }
 
     public static void notifyGitCommand(Project project, String command) {
-        notify(NotificationType.INFORMATION, NONE, project, "Git command:", command);
+        notify(NotificationType.WARNING, NONE, project, "Git command:", command);
     }
 
     private static void notify(NotificationType type, NotificationGroup group, Project project, String title, String message) {
